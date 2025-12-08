e253af77e26c443408f6d233efc85448805a9953
Victor Ejiofor <112650296+mufisiru002@users.noreply.github.com>
2025-12-08 10:00:42 +0100

    Update name from 'OLUWASEN JESSE' to 'OLUSANYA JAMES'

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -30,7 +30,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> OLUWASEN JESSE</strong>
+                <strong className="main-name"> OLUSANYA JAMES </strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
