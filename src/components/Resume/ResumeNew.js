6c187dc9616a2b4f4d044107de37aef3b69dc467
Victor Ejiofor <112650296+mufisiru002@users.noreply.github.com>
2025-12-09 23:42:12 +0100

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -17,10 +17,7 @@ function ResumeNew() {
 
   return (
     <div>
-      // <Container fluid className="resume-section">
-      //   <Particle />
-      //   <Row style={{ justifyContent: "center", position: "relative" }}>
-      //     <Button
+      // 
       //       variant="primary"
       //       href={pdf}
       //       target="_blank"
